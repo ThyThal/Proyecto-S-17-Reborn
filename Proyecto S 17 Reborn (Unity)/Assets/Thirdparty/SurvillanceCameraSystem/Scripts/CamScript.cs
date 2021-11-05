de29e2165fbc69b95dec0f35f01213caf4bcaf07
darkstryller <83669103+darkstryller@users.noreply.github.com>
2021-11-05 18:31:46 -0300

    jk

---
+++ Proyecto S 17 Reborn (Unity)/Assets/Thirdparty/SurvillanceCameraSystem/Scripts/CamScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CamScript : MonoBehaviour
+{
+	[SerializeField]
+	RenderTexture survillanceCameraTexture;  
+	[SerializeField]
+	Camera survillanceCamera; 
+	
+
+	
+	public IEnumerator SaveCameraView()
+	{
+		yield return new WaitForEndOfFrame();
+		
+		
+		RenderTexture rendText = RenderTexture.active;
+		RenderTexture.active = survillanceCamera.targetTexture;
+		
+		
+		survillanceCamera.Render();
+		
+		
+		Texture2D cameraImage = new Texture2D(survillanceCamera.targetTexture.width, survillanceCamera.targetTexture.height, TextureFormat.RGB24, false);
+		cameraImage.ReadPixels(new Rect(0, 0, survillanceCamera.targetTexture.width, survillanceCamera.targetTexture.height), 0, 0);
+		cameraImage.Apply();
+		RenderTexture.active = rendText;
+		
+		byte[] bytes = cameraImage.EncodeToPNG();
+		
+		System.IO.File.WriteAllBytes(Application.persistentDataPath + "/camera_image.png", bytes);
+	}
+}
