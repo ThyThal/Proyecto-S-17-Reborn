94e41d9a7769207bd5097cd0921c7c0c3fdb877b
Alejo Gaitán <thy.gaitan@gmail.com>
2021-12-17 16:36:10 -0300

    gas finished

--- Proyecto S 17 Reborn (Unity)/Assets/BossManager.cs
+++ Proyecto S 17 Reborn (Unity)/Assets/BossManager.cs
@@ -12,17 +12,41 @@ public class BossManager : MonoBehaviour
     [SerializeField] private GameObject _checkpoint;
 
     [SerializeField] private GameObject _bossDefense;
+    [SerializeField] private ParticleSystem _gas;
+
+    private Battery _playerBattery;
+    private ThirdPersonController _playerController;
+
+    [Header("Damage Player")]
+    [SerializeField] private bool activateGas = false;
+    [SerializeField] private float damageTimer = 1f;
+    [SerializeField] private float originalDamageTimer;
+    [SerializeField] private float batteryDamage;
+
+    private void Awake()
+    {
+        _playerBattery = _player.GetComponent<Battery>();
+        _playerController = _player.GetComponent<ThirdPersonController>();
+    }
+
+    private void Start()
+    {
+        originalDamageTimer = damageTimer;
+        _gas.Stop();
+    }
 
     public void DeactivateDefense()
     {
         _turret.isWeak = true;
         _bossDefense.SetActive(false);
+        _gas.Play();
     }
 
     public void ActivateDefense()
     {
         _turret.isWeak = false;
         _bossDefense.SetActive(true);
+        _gas.Stop();
     }
 
     public void KilledBoss()
@@ -36,4 +60,33 @@ public class BossManager : MonoBehaviour
         _turret.Activate();
     }
 
+    private void Update()
+    {
+        activateGas = _turret.isWeak;
+
+        if (_turret.isWeak)
+        {
+            damageTimer -= Time.deltaTime;
+
+            DamagePlayer();
+        }
+    }
+
+    private void DamagePlayer()
+    {
+        if (damageTimer <= 0)
+        {
+            if (_playerController.IsProtected)
+            {
+                _playerBattery.DamageBattery(batteryDamage/3);
+            }
+
+            else
+            {
+               _playerBattery.DamageBattery(batteryDamage);
+            }
+
+            damageTimer = originalDamageTimer;
+        }
+    }
 }
